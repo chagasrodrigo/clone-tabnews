947b44c8d256b7f8644dab037c99e1d3898f2d9f
Rodrigo Chagas <chagasrodrigo@gmail.com>
2025-12-27 17:59:26 -0300

    finish `/api/v1/status` endpoint

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,9 +1,51 @@
 import database from "../../../../infra/database.js";
 
-async function status(req, res) {
-  const result = await database.query("SELECT 1 + 1 AS sum;");
-  console.log(result);
-  res.status(200).json({ ok: true, sum: result.rows[0].sum });
+async function status(request, response) {
+  //const result = await database.query("SELECT 1 + 1 AS sum;");
+  //console.log(result);
+  const updatedAt = new Date().toISOString();
+
+  const databaseName = process.env.POSTGRES_DB; // request.query.databaseName; //"local_db";
+  console.log(`Banco de dados solicitado: ${databaseName}`);
+
+  const databaseOpenedConnectionsResult = await database.query({
+    //`SELECT COUNT(*)::int FROM pg_stat_activity WHERE datname = '${databaseName}';`,
+    text: "SELECT COUNT(*)::int FROM pg_stat_activity WHERE datname = $1;",
+    values: [databaseName],
+  });
+
+  const databaseVersionResult = await database.query("SHOW server_version;");
+  const databaseVersionValue = databaseVersionResult.rows[0].server_version;
+
+  const databaseMaxConnectionResult = await database.query(
+    "SHOW max_connections;",
+  );
+
+  const databaseMaxConnectionsValue = parseInt(
+    databaseMaxConnectionResult.rows[0].max_connections,
+  );
+
+  const databaseOpenedConnectionsValue =
+    databaseOpenedConnectionsResult.rows[0].count;
+  console.log(`Conexões abertas: ${databaseOpenedConnectionsValue}`);
+
+  response.status(200).json({
+    updated_at: updatedAt,
+    dependencies: {
+      database: databaseVersionValue,
+      //current_users: res.rows[0].current_users,
+      max_connections: parseInt(databaseMaxConnectionsValue),
+    },
+  });
+
+  console.log(response.status);
+
+  // server.js ou app.js
+  if (!process.listenerCount("SIGTERM")) {
+    process.on("SIGTERM", () => {
+      console.log("Encerrando servidor…");
+    });
+  }
 }
 
 export default status;
--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,4 +1,18 @@
 test("GET to /api/v1/status returns status 200 and correct body", async () => {
   const response = await fetch("http://192.168.18.7:3000/api/v1/status");
   expect(response.status).toBe(200);
+
+  const responseBody = await response.json();
+  expect(responseBody.updated_at).toBeDefined();
+
+  //Converter Data
+  const parsedUpdatedAt = new Date(responseBody.updated_at).toISOString();
+  expect(responseBody.updated_at).toEqual(parsedUpdatedAt);
+
+  expect(responseBody.dependencies.database).toEqual("16.0");
+
+  expect(responseBody.dependencies.max_connections).toBeGreaterThan(0);
+  //expect(responseBody.current_users).toBeGreaterThanOrEqual(0);
+
+  console.log(responseBody);
 });
